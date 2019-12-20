b9874ee6c7898eee9c298aee2931424db30a7015
G7495x <G7495x@gmail.com>
2019-12-19 20:45:41 -0800

    React EnhancedComponent - Removed class-properties babel plugin requirement

--- Helper/React/EnhancedComponent.js
+++ Helper/React/EnhancedComponent.js
@@ -1,10 +1,12 @@
-import React,{ Component } from 'react'
+import React,{Component} from 'react'
 
 export default class EnhancedComponent extends Component{
 	// Used for simple 2-way binding
 	// eg: <input {...this.twoWayBind('stateVariable')}/>
-	twoWayBind=(property,event='onChange',valAttr='value')=>({
-		[valAttr]:this.state[property],
-		[event]:e=>this.setState({[property]:e.target[valAttr]}),
-	})
+	twoWayBind(property,event='onChange',valAttr='value'){
+		return {
+			[valAttr]:this.state[property],
+			[event]:e=>this.setState({[property]:e.target[valAttr]}),
+		}
+	}
 }
